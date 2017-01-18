ba5819ae4376ace57fc4317a9f16ae36defc7b9d
Roi <roizundak@Gmail.com>
2017-01-18 20:01:47 +0200

    2.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'pages/index'
-  root 'pages#index'
 
   resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
