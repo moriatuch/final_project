2f05bcdb1e052536108270f0cb831e8b567ca9ec
Roi <roizundak@Gmail.com>
2017-01-18 20:31:41 +0200

    refactor photos to myApp

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'pages/index'
   root 'pages#index'
 
-  resources :photos
+  resources :myApp
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
