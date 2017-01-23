201874b024343400a1a1a15d071087eed910ef86
Roi <roizundak@Gmail.com>
2017-01-23 21:11:48 +0200

    adding layout to routs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :photos
   resources :pages
-
+  resources :layout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
