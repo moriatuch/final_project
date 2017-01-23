a8c1e3e523eb678c2e0f38b1fbf49ba606bd7ce3
Roi <roizundak@Gmail.com>
2017-01-23 21:38:23 +0200

    delete c

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :photos
   resources :pages
-  resources :layouts
+  resources :test
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
