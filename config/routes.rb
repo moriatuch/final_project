7b046f7483c45c51bf896885ca837394ba48dcc7
Roi <roizundak@Gmail.com>
2017-05-08 22:01:12 +0300

    Work on routs

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,11 @@ Rails.application.routes.draw do
 
   root 'sessions#new'
 
+  get 'uploads/count_cells'
 
+  get 'uploads/compare_treatment'
+
+  get 'uploads/dna_damage'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
