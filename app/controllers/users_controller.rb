99e544a85e9f8bee825dae34cb529034fafe34ff
Roi <roizundak@Gmail.com>
2017-05-10 02:29:38 +0300

    Add all  tables to manager profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @dnas = Dna.all
+    @counts = Count.all
+    @compares = Compare.all
   end
 
   def new
