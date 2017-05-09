967b10e800010b945199968f6c421e2418787c31
Roi <roizundak@Gmail.com>
2017-05-09 16:20:30 +0300

    Adding count Controllers

---
+++ app/controllers/counts_controller.rb
@@ -0,0 +1,32 @@
+class CountsController < ApplicationController
+  def index
+    @counts = Count.all
+  end
+
+  def new
+    @count = Count.new
+  end
+
+  def create
+    @count = Count.new(count_params)
+
+    if @count.save
+      redirect_to counts_path, notice: "The image #{@count.name} has been uploaded."
+    else
+      render "new"
+    end
+
+  end
+
+  def destroy
+    @count = Count.find(params[:id])
+    @count.destroy
+    redirect_to counts_path, notice:  "The image #{@count.name} has been deleted."
+  end
+
+  private
+  def count_params
+    params.require(:count).permit(:name, :attachment)
+  end
+
+end
---
+++ db/migrate/20170509125523_create_counts.rb
@@ -0,0 +1,10 @@
+class CreateCounts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :counts do |t|
+      t.string :name
+      t.string :attachment
+
+      t.timestamps
+    end
+  end
+end
