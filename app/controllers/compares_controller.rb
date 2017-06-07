df476f9b533f1e6ee137ead53f7c657df2eafa60
ruby -v <moriyatu@gmail.com>
2017-06-07 17:06:50 +0000

    fix the second algoritem #2

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -21,7 +21,7 @@ class ComparesController < ApplicationController
   def create
     `cp #{params[:compare][:attachment].tempfile.path} C:/Sites/final_project/matlab/compare_temp.png`
     Dir.chdir('/Sites/final_project/matlab')
-    `matlab -nodesktop -wait  -r "compare_ver2('compare_temp.png')";quit`
+    `matlab -nodesktop -wait  -r "compare_ver3('compare_temp.png')";quit`
     output = File.read('myfile.txt')
     @compare = Compare.new(compare_params)
     @compare.user_id = session["user_id"]
